b4f7c343ca024691ae342468e57d851ea46fc723
Oreoluwa Sokunbi <osokunbi@umich.edu>
2024-05-14 11:11:11 -0400

    refactored toast component and hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,63 @@
-import Toast from './Toast'
-import useGenerateRandomColor from './useGenerateRandomColor'
-import { useState, useRef, useEffect } from 'react'
-import  useToast from './useToast'
-import './App.css'
+import { useEffect, useRef, useState } from 'react';
+import { useToast } from './ToastContext';
+import './App.css';
+import useGenerateRandomColor from './useGenerateRandomColor';
 
 const App = () => {
-  const [color, generateColor] = useGenerateRandomColor('00000')
-  const [isPartying, setIsPartying] = useState(false)
-  const intervalRef = useRef(null)
-  const { toasts, toast } = useToast();
+  const [color, generateColor] = useGenerateRandomColor('00000');
+  const [isPartying, setIsPartying] = useState(false);
+  const intervalRef = useRef(null);
+  const { toast } = useToast();
 
   useEffect(() => {
-    generateColor()
-  }, [])
+    generateColor();
+  }, []);
+
+  useEffect(() => {
+    if (isPartying) {
+      intervalRef.current = setInterval(() => {
+        generateColor();
+      }, 300);
+    } else {
+      clearInterval(intervalRef.current);
+    }
+    return () => clearInterval(intervalRef.current);
+  }, [isPartying, generateColor]);
 
   const handleClick = () => {
-      toast({
-        title: "Copied to Clipboard",
-        description: "The color has been copied to your clipboard",
-      });
-      navigator.clipboard.writeText(`#${color}`);
-    };
+    toast({
+      title: 'Copied to Clipboard',
+      description: 'The color has been copied to your clipboard',
+    });
+    navigator.clipboard.writeText(`#${color}`);
+  };
 
   const styles = {
     backgroundColor: `#${color}`,
-    transition: 'background-color 0.5s ease'
-  }
+    transition: 'background-color 0.5s ease',
+  };
 
   const party = () => {
-    setIsPartying(!isPartying)
-    if (!isPartying) {
-      intervalRef.current = setInterval(() => {
-        generateColor()
-      }, 300)
-    } else {
-      clearInterval(intervalRef.current)
-    }
-  }
-
+    setIsPartying(!isPartying);
+  };
 
   return (
-      <div
-        style={styles}
-        className="App"
-        onClick={generateColor}
+    <div style={styles} className="App" onClick={generateColor}>
+      <button
+        id="colorSelector"
+        className="colorSelector"
+        onClick={(e) => {
+          e.stopPropagation();
+          handleClick();
+        }}
       >
-        <button
-          id="colorSelector"
-          className="colorSelector"
-          onClick={(e) => {
-            e.stopPropagation()
-            handleClick()}
-          }
-        >
-          <h1>{`#${color}`}</h1>
-        </button>
-        <button
-          onClick={party}
-          className="party">
-          <h2>{isPartying ? 'Stop the Party!' : 'Party Time!'}</h2>
-        </button>
-        <Toast toasts={toasts} />
-      </div>
-  )
-}
+        <h1>{`#${color}`}</h1>
+      </button>
+      <button onClick={party} className="party">
+        <h2>{isPartying ? 'Stop the Party!' : 'Party Time!'}</h2>
+      </button>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/Toast.jsx
+++ src/Toast.jsx
@@ -1,15 +1,50 @@
+import { useMemo, useState } from 'react';
 import './Toast.css';
+import { ToastContext } from './ToastContext';
 
-const Toast = ({ toasts }) => {
+export function ToastProvider({ children }) {
+  const [toasts, setToasts] = useState([]);
+
+  const toast = ({ title, description }) => {
+    const newToast = {
+      id: Date.now(),
+      title,
+      description,
+    };
+    setToasts((prevToasts) => [...prevToasts, newToast]);
+
+    setTimeout(() => hideToast(newToast.id), 3000);
+  };
+
+  const hideToast = (id) => {
+    setToasts((prevToasts) => prevToasts.filter((toast) => toast.id !== id));
+  };
+
+  const contextValue = useMemo(() => ({ toasts, toast }), [toasts]);
   return (
-    <div className="toasts-container">
-      {toasts.map((toast) => (
-        <div key={toast.id} className="Toast">
-          <h1>{toast.title}</h1>
-          <p>{toast.description}</p>
-        </div>
-      ))}
-    </div>
+    <ToastContext.Provider value={contextValue}>
+      {children}
+      <div className="toasts-container">
+        {toasts.length !== 0 &&
+          toasts.map((toast) => (
+            <Toast
+              key={toast.id}
+              id={toast.id}
+              title={toast.title}
+              description={toast.description}
+            />
+          ))}
+      </div>
+    </ToastContext.Provider>
+  );
+}
+
+const Toast = ({ id, title, description }) => {
+  return (
+      <div key={id} className="Toast">
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </div>
   );
 };
 
